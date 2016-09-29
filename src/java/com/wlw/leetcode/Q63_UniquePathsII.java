1f7cef634cedd2211c87004ae30e07638bfa7ebb
stanley <stanley@aduu.cn>
2016-09-29 14:20:11 +0800

    63

--- src/java/com/wlw/leetcode/Q63_UniquePathsII.java
+++ src/java/com/wlw/leetcode/Q63_UniquePathsII.java
@@ -16,23 +16,23 @@ public class Q63_UniquePathsII {
 		System.out.println(result);
 	}
 	public int uniquePathsWithObstacles(int[][] obstacleGrid) {
-		int n = obstacleGrid.length;
-		int m = obstacleGrid[0].length;
-		if(obstacleGrid[n-1][m-1] == 1)	{
+		int m = obstacleGrid.length;
+		int n = obstacleGrid[0].length;
+		if(obstacleGrid[m-1][n-1] == 1)	{
 			return 0;
 		}
-		int[] result = new int[m];
+		int[] result = new int[n];
 		boolean rightCanGo = true;
-		for(int i=1;i<=n;i++)	{
+		for(int i=m-1;i>=0;i--)	{
 			int temp = 0;
-			result[m-1] = 1;
-			if(obstacleGrid[n-i][m-1] == 1 || !rightCanGo)	{
-				result[m-1] = 0;
+			result[n-1] = 1;
+			if(obstacleGrid[i][n-1] == 1 || !rightCanGo)	{
+				result[n-1] = 0;
 				rightCanGo = false;
 			}
-			for(int j=m-1;j>0;j--)	{
+			for(int j=n-1;j>0;j--)	{
 				temp = result[j-1];
-				if(obstacleGrid[n-i][j-1] == 1)	{
+				if(obstacleGrid[i][j-1] == 1)	{
 					result[j-1] = 0;
 				} else {
 					result[j-1]  = temp+result[j];
