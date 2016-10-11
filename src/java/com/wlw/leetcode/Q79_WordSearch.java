82d4cc636e3b372d61a7710884d29656831dccac
stanley <stanley@aduu.cn>
2016-10-11 20:05:13 +0800

    79

--- src/java/com/wlw/leetcode/Q79_WordSearch.java
+++ src/java/com/wlw/leetcode/Q79_WordSearch.java
@@ -1,6 +1,8 @@
 package com.wlw.leetcode;
 
 import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Q79_WordSearch {
 
@@ -9,11 +11,11 @@ public class Q79_WordSearch {
 		
 		Date start = new Date();
 		
-		char[][] board = new char[][]{  {'A','B','C','S'},
-				  						{'B','F','C','E'},
-				  						{'C','S','E','E'}};
+		char[][] board = new char[][]{  {'A','B','C','E'},
+				  						{'S','F','E','S'},
+				  						{'A','D','E','E'}};
 		//SEE  ABCB
-		Object result = test.exist(board,"ABCCES");
+		Object result = test.exist(board,"ABCESEEEFA");
 
 		Date end = new Date();
 		System.out.println("time:"+ (end.getTime()-start.getTime()));
@@ -22,19 +24,49 @@ public class Q79_WordSearch {
 	public boolean exist(char[][] board, String word) {
 		int m=board.length;
         int n=board[0].length;
-        int end = m*n-1;
-        int pos = 0;
-        for(int k=0;k<=end;k++)	{
-        	int i = k/n;
-        	int j = k-n*i;
-        	if(board[i][j] == word.charAt(pos))	{
-        		pos++;
-        		if(pos == word.length())	{
-        			return true;
-        		}
+        
+        boolean suc = false;
+        for(int i=0;i<m;i++)	{
+        	for(int j=0;j<n;j++)	{
+        		if(board[i][j] == word.charAt(0))	{
+        			Set<Integer> gone = new HashSet<Integer>();
+            		suc = suc || go(board, m, n, i, j, word, gone);
+            	}
         	}
         }
-        return false;
+        return suc;
     }
+	
+	public boolean go(char[][] board,int m,int n,int startRow,int startCol,String word,Set<Integer> gone)	{
+		if(word.length() == 1 && board[startRow][startCol] == word.charAt(0))	{
+			System.out.println(gone);
+			return true;
+		}
+		if(board[startRow][startCol] != word.charAt(0))	{
+			return false;
+		}
+		gone.add(startRow*n+startCol);
+		
+		boolean suc = false;
+		//向右
+		if(startCol < n-1 && !gone.contains(startRow*n+startCol+1))	{
+			suc = suc || go(board, m, n, startRow, startCol+1, word.substring(1), gone);
+		}
+		//向左
+		if(startCol > 0 && !gone.contains(startRow*n+startCol-1))	{
+			suc = suc || go(board, m, n, startRow, startCol-1, word.substring(1), gone);
+		}
+		
+		//向下
+		if(startRow < m-1 && !gone.contains((startRow+1)*n+startCol))	{
+			suc = suc || go(board, m, n, startRow+1, startCol, word.substring(1), gone);
+		}
+		//向上
+		if(startRow > 0 && !gone.contains((startRow-1)*n+startCol))	{
+			suc = suc || go(board, m, n, startRow-1, startCol, word.substring(1), gone);
+		}
+		gone.remove(startRow*n+startCol);
+		return suc;
+	}
 }
 
