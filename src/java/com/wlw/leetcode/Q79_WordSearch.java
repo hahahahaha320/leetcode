8ea0f794bf5f6f4e84b6cf8b2abd2d8712a8c873
stanley <108452287@qq.com>
2016-10-11 22:39:36 +0800

    80

--- src/java/com/wlw/leetcode/Q79_WordSearch.java
+++ src/java/com/wlw/leetcode/Q79_WordSearch.java
@@ -1,8 +1,6 @@
 package com.wlw.leetcode;
 
 import java.util.Date;
-import java.util.HashSet;
-import java.util.Set;
 
 public class Q79_WordSearch {
 
@@ -15,7 +13,7 @@ public class Q79_WordSearch {
 				  						{'S','F','E','S'},
 				  						{'A','D','E','E'}};
 		//SEE  ABCB
-		Object result = test.exist(board,"A");
+		Object result = test.exist(board,"ABCESEEEF");
 
 		Date end = new Date();
 		System.out.println("time:"+ (end.getTime()-start.getTime()));
@@ -29,7 +27,7 @@ public class Q79_WordSearch {
         for(int i=0;i<m;i++)	{
         	for(int j=0;j<n;j++)	{
         		if(board[i][j] == word.charAt(0))	{
-        			Set<Integer> gone = new HashSet<Integer>();
+        			boolean[][] gone = new boolean[m][n];
             		suc = suc || go(board, m, n, i, j, word, gone);
             	}
         	}
@@ -37,38 +35,38 @@ public class Q79_WordSearch {
         return suc;
     }
 	
-	public boolean go(char[][] board,int m,int n,int startRow,int startCol,String word,Set<Integer> gone)	{
+	public boolean go(char[][] board,int m,int n,int startRow,int startCol,String word,boolean[][] gone)	{
 		if(word.length() == 1 && board[startRow][startCol] == word.charAt(0))	{
 			return true;
 		}
 		if(board[startRow][startCol] != word.charAt(0))	{
 			return false;
 		}
-		gone.add(startRow*n+startCol);
+		gone[startRow][startCol]=true;
 		
 		boolean suc = false;
 		//向右
-		if(startCol < n-1 && !gone.contains(startRow*n+startCol+1)
+		if(startCol < n-1 && !gone[startRow][startCol+1]
 				&& board[startRow][startCol+1] ==  word.charAt(1))	{
 			suc = suc || go(board, m, n, startRow, startCol+1, word.substring(1), gone);
 		}
 		//向左
-		if(startCol > 0 && !gone.contains(startRow*n+startCol-1)
+		if(startCol > 0 && !gone[startRow][startCol-1]
 				&& board[startRow][startCol-1] ==  word.charAt(1))	{
 			suc = suc || go(board, m, n, startRow, startCol-1, word.substring(1), gone);
 		}
 		
 		//向下
-		if(startRow < m-1 && !gone.contains((startRow+1)*n+startCol)
+		if(startRow < m-1 && !gone[startRow+1][startCol]
 				&& board[startRow+1][startCol] ==  word.charAt(1))	{
 			suc = suc || go(board, m, n, startRow+1, startCol, word.substring(1), gone);
 		}
 		//向上
-		if(startRow > 0 && !gone.contains((startRow-1)*n+startCol)
+		if(startRow > 0 && !gone[startRow-1][startCol]
 				&& board[startRow-1][startCol] ==  word.charAt(1))	{
 			suc = suc || go(board, m, n, startRow-1, startCol, word.substring(1), gone);
 		}
-		gone.remove(startRow*n+startCol);
+		gone[startRow][startCol]=false;
 		return suc;
 	}
 }
---
+++ src/java/com/wlw/leetcode/Q80_RemoveDuplicatesfromSortedArrayII.java
@@ -0,0 +1,40 @@
+package com.wlw.leetcode;
+
+import java.util.Date;
+
+public class Q80_RemoveDuplicatesfromSortedArrayII {
+
+	public static void main(String[] args) {
+		Q80_RemoveDuplicatesfromSortedArrayII test = new Q80_RemoveDuplicatesfromSortedArrayII();
+		
+		Date start = new Date();
+		
+		int[] nums = new int[]{1,1,1,2,2,3};
+		//SEE  ABCB
+		Object result = test.removeDuplicates(nums);
+
+		Date end = new Date();
+		System.out.println("time:"+ (end.getTime()-start.getTime()));
+		System.out.println(result);
+		Util.printArray(nums);
+	}
+	public int removeDuplicates(int[] nums) {
+		int result = 1;
+		int num = 1;
+		int cur = 1;
+        for(int i=1;i<nums.length;i++)	{
+        	if(nums[i] == nums[i-1])	{
+        		num++;
+        	} else {
+        		num = 1;
+        	}
+        	nums[cur] = nums[i];
+        	if(num < 3)	{
+        		result++;
+        		cur++;
+        	}
+        }
+        return result;
+    }
+}
+
