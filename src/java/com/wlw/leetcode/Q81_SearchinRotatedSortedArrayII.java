8e602c2dec97e3c5afed72437d22116af291e18e
stanley <stanley@aduu.cn>
2016-10-12 17:11:57 +0800

    84

--- src/java/com/wlw/leetcode/Q81_SearchinRotatedSortedArrayII.java
+++ src/java/com/wlw/leetcode/Q81_SearchinRotatedSortedArrayII.java
@@ -2,27 +2,27 @@ package com.wlw.leetcode;
 
 import java.util.Date;
 
-public class Q33_SearchinRotatedSortedArray {
+public class Q81_SearchinRotatedSortedArrayII {
 
 	public static void main(String[] args) {
-		Q33_SearchinRotatedSortedArray test = new Q33_SearchinRotatedSortedArray();
+		Q81_SearchinRotatedSortedArrayII test = new Q81_SearchinRotatedSortedArrayII();
 		
 		Date start = new Date();
 		
 		int[] nums = new int[]{6,7,9,1,4,5};
-		int result = test.search(nums,9);
+		Object result = test.search(nums,9);
 		
 		Date end = new Date();
 		System.out.println("time:"+ (end.getTime()-start.getTime()));
 		System.out.println(result);
 	}
-	public int search(int[] nums, int target) {
+	public boolean search(int[] nums, int target) {
 		for(int i=0;i<nums.length;i++)	{
 			if(nums[i] == target)	{
-				return i;
+				return true;
 			}
 		}
-        return -1;
+        return false;
     }
     
 }
---
+++ src/java/com/wlw/leetcode/Q83_RemoveDuplicatesfromSortedList.java
@@ -0,0 +1,36 @@
+package com.wlw.leetcode;
+
+import java.util.Date;
+
+public class Q83_RemoveDuplicatesfromSortedList {
+
+	public static void main(String[] args) {
+		Q83_RemoveDuplicatesfromSortedList test = new Q83_RemoveDuplicatesfromSortedList();
+		
+		Date start = new Date();
+		
+		int[] nums = new int[]{1,2,2};
+		Object result = test.deleteDuplicates(ListNode.makeList(nums));
+		
+		Date end = new Date();
+		System.out.println("time:"+ (end.getTime()-start.getTime()));
+		System.out.println(result);
+	}
+	public ListNode deleteDuplicates(ListNode head) {
+		if(head == null)	{
+			return head;
+		}
+		ListNode newHead = new ListNode(head.val-1);
+		ListNode newLast = newHead;
+		ListNode cur = head;
+		while(cur != null)	{
+			if(cur.next == null || cur.val != cur.next.val)	{
+				newLast.next = new ListNode(cur.val);
+				newLast = newLast.next;
+			}
+			cur = cur.next;
+		}
+		return newHead.next;
+    }
+}
+
