0eba1ad3eb4e819fe99c3afa619f0604d1975a55
stanley <stanley@aduu.cn>
2016-10-25 14:36:27 +0800

    100_over

--- src/java/com/wlw/leetcode/Q99_RecoverBinarySearchTree.java
+++ src/java/com/wlw/leetcode/Q99_RecoverBinarySearchTree.java
@@ -1,6 +1,12 @@
 package com.wlw.leetcode;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Stack;
 
 public class Q99_RecoverBinarySearchTree {
 
@@ -8,13 +14,13 @@ public class Q99_RecoverBinarySearchTree {
 		Q99_RecoverBinarySearchTree test = new Q99_RecoverBinarySearchTree();
 		
 		Date start = new Date();
-		TreeNode node1 = new TreeNode(10);
-		TreeNode node2 = new TreeNode(5);
-		TreeNode node3 = new TreeNode(20);
-		TreeNode node4 = new TreeNode(2);
-		TreeNode node5 = new TreeNode(7);
-		TreeNode node6 = new TreeNode(15);
-		TreeNode node7 = new TreeNode(25);
+		TreeNode node1 = new TreeNode(1);
+		TreeNode node2 = new TreeNode(2);
+		TreeNode node3 = new TreeNode(3);
+		TreeNode node4 = new TreeNode(4);
+		TreeNode node5 = new TreeNode(5);
+		TreeNode node6 = new TreeNode(6);
+		TreeNode node7 = new TreeNode(7);
 		
 		node1.left = node2;
 		node1.right = node3;
@@ -29,8 +35,59 @@ public class Q99_RecoverBinarySearchTree {
 		System.out.println("time:"+ (end.getTime()-start.getTime()));
 		System.out.println(node1);
 	}
+	
+	
 	public void recoverTree(TreeNode root) {
-        
+		if(root==null)	{
+			return;
+		}
+        List<Integer> midOrderList = midOrder(root);
+        Collections.sort(midOrderList);
+        recoverTreeRec(root,midOrderList,0,new HashMap<TreeNode,Integer>());
+    }
+	
+	public void recoverTreeRec(TreeNode root,List<Integer> orderList,int start,Map<TreeNode,Integer> cache) {
+        int leftClildNum = nodeNum(root.left,cache);
+        root.val = orderList.get(start+leftClildNum);
+        if(root.left != null)	{
+        	recoverTreeRec(root.left, orderList, start,cache);
+        }
+        if(root.right != null)	{
+        	recoverTreeRec(root.right, orderList, start+leftClildNum+1,cache);
+        }
+	}
+	
+	public List<Integer> midOrder(TreeNode root) {
+		List<Integer> result = new ArrayList<Integer>();
+		if(root == null)	{
+			return result;
+		}
+		Stack<TreeNode> s = new Stack<TreeNode>();
+		TreeNode cur = root;
+	    while(cur!=null||!s.empty()) {
+	        while(cur!=null) {
+	            s.push(cur);
+	            cur=cur.left;
+	        }
+	        if(!s.empty()) {
+	        	cur = s.pop();
+	        	result.add(cur.val);
+	        	cur = cur.right;
+	        }
+	    }
+	    return result;
+    }
+	
+	public int nodeNum(TreeNode root,Map<TreeNode,Integer> cache) {
+		if(root == null)	{
+			return 0;
+		}
+		if(cache.get(root) != null)	{
+			return cache.get(root);
+		}
+	    int num = nodeNum(root.left,cache)+nodeNum(root.right,cache)+1;
+	    cache.put(root,num);
+	    return num;
     }
 }
 
---
+++ src/java/com/wlw/leetcode/Q99_RecoverBinarySearchTree4.java
@@ -0,0 +1,52 @@
+package com.wlw.leetcode;
+
+import java.util.Date;
+
+public class Q99_RecoverBinarySearchTree4 {
+
+	public static void main(String[] args) {
+		Q99_RecoverBinarySearchTree4 test = new Q99_RecoverBinarySearchTree4();
+		
+		Date start = new Date();
+		TreeNode node1 = new TreeNode(2);
+		TreeNode node2 = new TreeNode(3);
+		TreeNode node3 = new TreeNode(1);
+		
+		node1.left = node2;
+		node1.right = node3;
+		
+		test.recoverTree(node1);
+		
+		Date end = new Date();
+		System.out.println("time:"+ (end.getTime()-start.getTime()));
+		System.out.println(node1);
+	}
+	TreeNode node1 = null;
+    TreeNode node2 = null;
+    public void recoverTree(TreeNode root) {
+        inorderTraverse(root);
+        int tmp = node1.val;
+        node1.val = node2.val;
+        node2.val = tmp;
+    }
+    
+    TreeNode prev = null;
+    private void inorderTraverse(TreeNode root) {
+        if(root == null)	{
+            return;
+        }
+        if(root.left != null)	{
+        	inorderTraverse(root.left);
+        }
+        if(prev != null && root.val <= prev.val) {
+            if (node1 == null) {
+                node1 = prev;
+            }
+            node2 = root;
+        }
+        prev = root;
+        if(root.right != null)	{
+        	inorderTraverse(root.right);
+        }
+    }
+}
