61baca55ad0c05769bd7c1256458f5be083a3ecb
stanley <stanley@aduu.cn>
2017-12-08 16:17:37 +0800

    115

--- src/java/com/wlw/leetcode2/Q103_BinaryTreeZigzagLevelOrderTraversal.java
+++ src/java/com/wlw/leetcode2/Q103_BinaryTreeZigzagLevelOrderTraversal.java
@@ -4,10 +4,10 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
-public class Q102_BinaryTreeLevelOrderTraversal {
+public class Q103_BinaryTreeZigzagLevelOrderTraversal {
 
 	public static void main(String[] args) {
-		Q102_BinaryTreeLevelOrderTraversal test = new Q102_BinaryTreeLevelOrderTraversal();
+		Q103_BinaryTreeZigzagLevelOrderTraversal test = new Q103_BinaryTreeZigzagLevelOrderTraversal();
 		
 		Date start = new Date();
 		
@@ -15,22 +15,28 @@ public class Q102_BinaryTreeLevelOrderTraversal {
 		TreeNode node2 = new TreeNode(2);
 		TreeNode node3 = new TreeNode(3);
 		
-		TreeNode node4 = new TreeNode(15);
-		TreeNode node5 = new TreeNode(7);
+		TreeNode node4 = new TreeNode(4);
+		TreeNode node5 = new TreeNode(5);
+		
+		TreeNode node6 = new TreeNode(6);
+		TreeNode node7 = new TreeNode(7);
 		
 		node1.left = node2;
 		node1.right = node3;
 		
-		node3.left = node4;
-		node3.right = node5;
+		node2.left = node4;
+		node2.right = node5;
 		
-		Object result = test.levelOrder(node1);
+		node3.left = node6;
+		node3.right = node7;
+		
+		Object result = test.zigzagLevelOrder(node1);
 		
 		Date end = new Date();
 		System.out.println("time:"+ (end.getTime()-start.getTime()));
 		System.out.println(result);
 	}
-	public List<List<Integer>> levelOrder(TreeNode root) {
+	public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
 		List<List<Integer>> result = new ArrayList<List<Integer>>();
 		if(root == null)	{
 			return result;
@@ -38,10 +44,16 @@ public class Q102_BinaryTreeLevelOrderTraversal {
 		List<TreeNode> toDealList = new ArrayList<TreeNode>();
         List<TreeNode> resultNodeList = new ArrayList<TreeNode>();
         toDealList.add(root);
+        boolean pos = true;
         while(toDealList.size() != 0)	{
         	List<Integer> subResult = new ArrayList<Integer>();
         	for(TreeNode node : toDealList)	{
-        		subResult.add(node.val);
+        		if(pos)	{
+        			subResult.add(node.val);
+        		} else {
+        			subResult.add(0,node.val);
+        		}
+        		
         		if(node.left != null)	{
         			resultNodeList.add(node.left);
         		}
@@ -54,6 +66,7 @@ public class Q102_BinaryTreeLevelOrderTraversal {
         	toDealList = resultNodeList;
         	resultNodeList = temp;
         	resultNodeList.clear();
+        	pos = !pos;
         }
         return result;
 	}
